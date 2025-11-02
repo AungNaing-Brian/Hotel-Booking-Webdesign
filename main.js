128fb899a2aeda8e0351079d13e7914fbd2a2f65
AungNaing <aungnaing.brina@gmail.com>
2025-11-03 03:14:41 +0400

    addedHeader

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 /* ========== MENU ========== */
 
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", () => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
 /* ========== SCROLLREVEAL ========== */
 /* ========== HEADER ========== */
+ScrollReveal().reveal(".header__container p", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__container h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
 
 /* ========== ABOUT ========== */
 
